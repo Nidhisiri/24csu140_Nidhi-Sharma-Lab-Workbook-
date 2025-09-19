0092122b64cf4b9687244d0ca91246dc322350da
Nidhisiri <24csu140@ncuindia.edu>
2025-09-20 00:57:48 +0530

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,33 @@
+class operation{
+public static void main(String args[]){
+
+System.out.println("Nidhi Sharma");
+System.out.println("24CSU140");
+
+int a=6;
+int b=3;
+int sum=a+b;
+int diff=a-b;
+int multiply=a*b;
+int divide=a/b;
+int modulus=a%b;
+System.out.println("First Number = " +a);
+System.out.println("Second Number = " +b);
+System.out.println("Sum = " +sum);
+System.out.println("Diff = " +diff);
+System.out.println("Multiply = " +multiply);
+System.out.println("Divide = " +divide);
+System.out.println("Modulus = " +modulus);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
